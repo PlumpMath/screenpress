457c848b017e989cf27797da3809216b2434e016
Calvin Lai <callai@gmail.com>
2015-11-25 22:13:57 -0500

    convert rehydrated state to Immutable

--- client/index.js
+++ client/index.js
@@ -5,15 +5,23 @@ import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute } from 'react-router';
 import { routes } from '../common/routes';
 import { syncReduxAndRouter } from 'redux-simple-router';
-import { createHistory } from 'history';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { fromJS } from 'immutable';
 import configureStore from '../common/store/configureStore';
 
 // stylesheets
 import '../common/assets/stylesheets/main.scss';
 
 const initialState = window.__INITIAL_STATE__;
+
+// Transform initial state from server to Immutable.js collections,
+// but leave top level keys untouched for Redux
+Object.keys(initialState).forEach(key => {
+  initialState[key] = fromJS(initialState[key]);
+});
+
 const store = configureStore(initialState);
-const history = createHistory();
+const history = createBrowserHistory();
 
 syncReduxAndRouter(history, store);
 
